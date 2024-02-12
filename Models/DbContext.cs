7e21c8ed9ef3956ba1115d5cc6909f8952699025
Yohan Fraga <101361742+yohanfraga@users.noreply.github.com>
2024-02-12 17:23:09 -0300

    add my project files

---
+++ Models/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitysTest.Models {
+    public class UsersContext : DbContext {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\yohan\\source\\repos\\BitysTest\\database\\Bit_Test.mdf;Integrated Security=True");
+        }
+    }
+}
